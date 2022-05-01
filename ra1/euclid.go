bc0db5625799a3b31946f456dc7cdb03bd036801
John Chen <jmc_tt@live.com>
2022-05-01 17:12:39 -0400

    Refactor EuclidGCD to use big Int.

--- ra1/euclid.go
+++ ra1/euclid.go
@@ -1,18 +1,27 @@
 package ra1
 
+import "math/big"
+
 // EuclidGCD returns the greatest common divisor of two positive integers, x and y.
-func EuclidGCD(x, y int) int {
-	if y == 0 {
+func EuclidGCD(x, y *big.Int) *big.Int {
+	if y.Cmp(big.NewInt(0)) == 0 {
 		return x
 	}
-	return EuclidGCD(y, x%y)
+	z := big.NewInt(0).Mod(x, y)
+	return EuclidGCD(y, z)
 }
 
 // ExtEuclid returns d = gcd(x,y), α = x^-1 mod y, β = y^-1 mod x
-func ExtEuclid(x, y int) (d, α, β int) {
-	if y == 0 {
-		return x, 1, 0
+func ExtEuclid(x, y *big.Int) (d, α, β *big.Int) {
+	if y.Cmp(big.NewInt(0)) == 0 {
+		return x, big.NewInt(1), big.NewInt(0)
 	}
-	d, α_prime, β_prime := ExtEuclid(y, x%y)
-	return d, β_prime, α_prime - (x/y)*β_prime
+
+	q := big.NewInt(0).Mod(x, y)
+	d, α_prime, β_prime := ExtEuclid(y, q)
+
+	r := big.NewInt(0).Div(x, y)
+	s := big.NewInt(0).Mul(r, β_prime)
+	t := big.NewInt(0).Add(α_prime, s)
+	return d, β_prime, t
 }
